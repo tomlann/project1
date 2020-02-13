dbabc28e153311f6c3cb2a406fc770498ce9bf03
tomlann <48773862+tomlann@users.noreply.github.com>
2020-02-13 10:09:01 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import os 
+
 from flask import Flask__
 from flask_sqlalchemy import SQLAlchemy
 
@@ -21,3 +23,6 @@ def create_app():
     app.register_blueprint(main_blueprint)
 
     return app
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
